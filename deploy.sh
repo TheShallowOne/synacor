99ff6a795983f8bc8cf9ef674b975ca3a8ea85b9
TheShallowOne <TheShallowOne@users.noreply.github.com>
2018-02-09 18:59:12 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -52,6 +52,8 @@ git commit -m "Deploy to GitHub Pages: ${SHA}"
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+echo $ENCRYPTED_KEY_VAR
+echo $ENCRYPTED_IV_VAR
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
