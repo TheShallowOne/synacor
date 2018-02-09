3d3c1f99c55d3e50ebc6aa98faa8cacee6b484af
TheShallowOne <TheShallowOne@users.noreply.github.com>
2018-02-09 23:39:42 +0100

    Grrr r

---
+++ scripts/copy_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    exit 0
+fi
+
+cp static/* out/
+cp ./target/wasm32-unknown-unknown/release/synacor_vm.wasm out/synacor_vm.wasm
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,16 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+   exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
 # Now let's go have some fun with the cloned repo
 cd out
 git config user.name "Travis CI"
